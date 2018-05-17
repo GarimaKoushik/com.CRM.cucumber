c0279c2fef0c58a6a87b458ffc5c04d6579c32d5
GarimaKoushik <38219342+GarimaKoushik@users.noreply.github.com>
2018-05-17 08:11:06 +0530

    Update TestRunner.java

--- src/main/java/Runner/TestRunner.java
+++ src/main/java/Runner/TestRunner.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features= "src/main/java/Features/",
-		glue= {"/src/main/java/stepDefinition/"},
+		glue= {"src/main/java/stepDefinition/"},
 		monochrome=false
 		
 		)
